38e9dc73f347300d02665e7003a2e5bd1c5a8ddf
Gassgs <71162352+Gassgs@users.noreply.github.com>
2021-02-14 15:03:49 -0600

    Create Mode Blinds-Shades-Curtains Parent App.groovy

---
+++ Apps/Mode Blinds-Shades-Curtains/Mode Blinds-Shades-Curtains Parent App.groovy
@@ -0,0 +1,72 @@
+/**
+ *  ****************  Mode Blinds-Shades-Curtains Parent App ****************
+ *
+ *  Mode Blinds-Shades-Curtains Parent App for Mode Blinds-Shades-Curtains Child
+ *  Mode blinds shades curtains app. Use this app to set postion based on mode.
+ *  motion option to activate plus Lux level, outdoor temp, contact sensors
+ *  and mode options.
+ *   
+ *
+ *
+ *  Copyright 2021 Gassgs / Gary Gassmann
+ *
+ *
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *-------------------------------------------------------------------------------------------------------------------
+ *
+ *
+ *-------------------------------------------------------------------------------------------------------------------
+ *
+ *  Last Update: 2/08/2021
+ *
+ *  Changes:
+ *
+ *  V1.0.0  -       2-13-2021       First Run 
+ *    
+ */
+
+import groovy.transform.Field
+
+definition(
+    name: "Mode Blinds-Shades-Curtains",
+    namespace: "Gassgs",
+    author: "Gary G",
+    description: "Mode Blinds-Shades-Curtains Parent App",
+    category: "",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: ""
+)
+
+preferences{
+    page(
+        name: "mainPreferences",
+        title: "<div style='text-align:center'><b>Mode Blinds-Shades-Curtains</b></div>",
+        install: true,
+        uninstall: true)
+        {
+        section{
+            app(
+                name: "childApps",
+                appName: "Mode Blinds-Shades-Curtains Child",
+                namespace: "Gassgs",
+                title: "<b>Add New Mode Blinds-Shades-Curtains Rule</b>",
+                multiple: true
+                )
+        }
+    }
+}
+
+def uninstalled(){
+    getChildApps().each { childApp ->
+        deleteChildApp(childApp.id)
+    }
+}
