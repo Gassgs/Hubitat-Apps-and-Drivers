0cd6536ff48c7a49c89916421cc5d8d955962559
Gassgs <71162352+Gassgs@users.noreply.github.com>
2021-11-02 15:36:39 -0500

    Rewrite Handlers to improve response time

--- Apps/Harmony IR Controlled Fireplace/Harmony Controlled Fireplace App.groovy
+++ Apps/Harmony IR Controlled Fireplace/Harmony Controlled Fireplace App.groovy
@@ -21,19 +21,22 @@
  *
  *-------------------------------------------------------------------------------------------------------------------
  *
- *  Last Update: 1/24/2021
+ *  Last Update: 10/31/2021
  *
  *  Changes:
  *
  *  V1.0.0 -        1-24-2021       First run
  *  V2.0.0 -        1-26-2021       Fixed all logic to sync attributes
  *  V2.1.0 -        2-04-2021       added outdoor temperature
+ *  V2.2.0 -        6-18-2021       Fixed Low and High Sync issue
+ *  V2.3.0 -        8-07-2021       Removed Color change (tasmota controlled RGB now)
+ *  V2.4.0 -        10-31-2021      Rewrite Handlers to improve response time
  */
 
 import groovy.transform.Field
 
 definition(
-    name: "Harmony Controlled Fireplace ",
+    name: "Harmony Controlled Fireplace",
     namespace: "Gassgs",
     author: "Gary G",
     description: "Harmony Controlled Fireplace app",
@@ -50,7 +53,7 @@ preferences {
      paragraph(
          title: "Harmony IR Controlled Fireplacce",
          required: false,
-    	"<div style='text-align:center'><b>: Bedroom Fireplace :</b></div>"
+    	"<div style='text-align:center'><b><big>: Bedroom Fireplace :</big></b></div>"
     	)
         paragraph( "<div style='text-align:center'><b>Power and Control options</b></div>"
         )
@@ -127,22 +130,10 @@ def updated(){
 
 def initialize(){
     logInfo ("Settings: ${settings}")
-    subscribe(settings.fireplace, "switch.on", fireplaceSwitchOnHandler)
-    subscribe(settings.fireplace, "switch.off", fireplaceSwitchOffHandler)
-    subscribe(settings.fireplace, "heat.off", fireplaceHeatOffHandler)
-    subscribe(settings.fireplace, "heat.low", fireplaceHeatLowHandler)
-    subscribe(settings.fireplace, "heat.high", fireplaceHeatHighHandler)
-    subscribe(settings.fireplace, "color.on", fireplaceColorHandler)
+    subscribe(settings.fireplace, "switch", switchHandler)
+    subscribe(settings.fireplace, "heat", fireplaceHeatHandler)
     subscribe(settings.powerMeter,"power", powerHandler)
     logInfo ("subscribed to Events")
-}
-
-def fireplaceSwitchOnHandler(evt){
-    fireplaceSwitch = evt.value
-    logInfo ("fireplace switch $fireplaceSwitch")
-    state.fireplaceSwitchOn = (fireplaceSwitch == "on")
-    turnFireplaceOn()
-    getTemperature()
 }
 
 def averageTemperature(){
@@ -154,113 +145,125 @@ def averageTemperature(){
 
 def getTemperature(){
 	def avg = averageTemperature()
-	logInfo ("Current temperature average is ${averageTemperature()}")
+	logInfo ("$app.label Current temperature average is ${averageTemperature()}")
         if (tempThreshold>avg){
-            logInfo ("outdoor Temp below threshold turning heat on low")
+            logInfo ("$app.label Outdoor Temp below threshold turning heat on low")
             setHeatLow()
     }
 }
 
-def fireplaceSwitchOffHandler(evt){
-    fireplaceSwitch = evt.value
-    logInfo ("fireplace switch $fireplaceSwitch")
-    state.fireplaceSwitchOff = (fireplaceSwitch == "off")
-    turnFireplaceOff()
+def switchHandler(evt){
+    switchStatus = evt.value
+    logInfo ("$app.label switch $switchStatus")
+    state.switchOn = (switchStatus == "on")
+    state.switchOff = (switchStatus == "off")
+    if (state.switchOn && state.powerOff){
+        turnFireplaceOn()
+        runIn(1,getTemperature)
+    }
+    else if (state.switchOff && state.powerOn){
+        turnFireplaceOff()
+    }
 }
 
-def fireplaceHeatHighHandler(evt){
-    highHeat = evt.value
-    logInfo ("fireplace heat $highHeat")
-    state.fireplaceSetHeatHigh = (highHeat == "high")
-    setHeatHigh()
+def fireplaceHeatHandler(evt){
+    heatLevel = evt.value
+    logInfo ("$app.label fireplace heat $heatLevel")
+    state.fireplaceHeatHigh = (heatLevel == "high")
+    state.fireplaceHeatLow = (heatLevel == "low")
+    state.fireplaceHeatOff = (heatLevel == "off")
+    if (state.fireplaceHeatHigh){
+        setHeatHigh()
+    }
+    else if (state.fireplaceHeatLow){
+        setHeatLow()
+    }
+    else if (state.fireplaceHeatOff){
+        setHeatOff()
+    }
+    runIn(2,powerSync)
 }
 
-def fireplaceHeatLowHandler(evt){
-    lowHeat = evt.value
-    logInfo ("fireplace heat $lowHeat")
-    state.fireplaceSetHeatHigh = (lowHeat == "low")
-    setHeatLow()
-}
-
-def fireplaceHeatOffHandler(evt){
-    offHeat = evt.value
-    logInfo ("fireplace heat $offHeat")
-    state.fireplaceSetHeatOff = (offHeat == "off")
-    setHeatOff()
-}
 
 def powerHandler(evt){
     meterValue = evt.value.toDouble()
-    state.fireplaceOff = (meterValue <( 3))
-    state.fireplaceOn = (meterValue >( 4))
-    state.fireplaceHeatOff = (meterValue  <( 20))
-    state.fireplaceHeatLow = (meterValue >(300)&&meterValue <(900))
-    state.fireplaceHeatHigh = (meterValue >( 900))
-    powerSync()
+    state.powerOff = (meterValue < (1.5))
+    state.powerOn = (meterValue >= (1.5))
+    state.heatPowerOff = (meterValue  <(300))
+    state.heatPowerLow = (meterValue >(300)&&meterValue <(900))
+    state.heatPowerHigh = (meterValue >( 900))
+    if (state.powerOff && state.switchOn){
+        runIn(2,powerSync)
+    }
+    if (state.powerOn && state.switchOff){
+        runIn(2,powerSync)
+    }
+    if(state.heatPowerOff && ! state.fireplaceHeatOff){
+        runIn(2,powerSync)
+    }
+    if (state.heatPowerLow && ! state.fireplaceHeatLow){
+        runIn(2,powerSync)
+    }
+    if (state.heatPowerHigh && ! state.fireplaceHeatHigh){
+        runIn(2,powerSync)
+    }    
 }
 
 def powerSync(){
-    if (state.fireplaceOff){
+    if (state.powerOff && state.switchOn){
         settings.fireplace.off()
     }
-    if (state.fireplaceOn){
+    if (state.powerOn && state.switchOff){
         settings.fireplace.on()
+        getTemperature()
     }
-    if(state.fireplaceHeatOff){
+    if(state.heatPowerOff && ! state.fireplaceHeatOff){
         settings.fireplace.setHeat("Off")
     }
-    if (state.fireplaceHeatLow){
+    if (state.heatPowerLow && ! state.fireplaceHeatLow){
         settings.fireplace.setHeat("Low")
     }
-    if (state.fireplaceHeatHigh){
+    if (state.heatPowerHigh && ! state.fireplaceHeatHigh){
         settings.fireplace.setHeat("High")
     }
-}
+} 
 
 def turnFireplaceOn(){
-    if (state.fireplaceOff){
-        settings.harmonyHub.deviceCommand("pwr","65347561")
-    }
+    logInfo ("$app.label turning ON")
+    settings.harmonyHub.deviceCommand("pwr","65347561")
 }
 
 def turnFireplaceOff(){
-    if (state.fireplaceOn){
-        settings.harmonyHub.deviceCommand("pwr","65347561")   
-    }
+    settings.harmonyHub.deviceCommand("pwr","65347561")
+    logInfo ("$app.label turning OFF")
 }
 
 def setHeatOff(){
-    if (state.fireplaceHeatLow){
+    if (state.heatPowerLow){
         settings.harmonyHub.deviceCommand("low","65347561")
     }
-    else if  (state.fireplaceHeatHigh){
+    else if  (state.heatPowerHigh){
         settings.harmonyHub.deviceCommand("high","65347561")
+    }else{
+        logInfo ("$app.label Heat already set to off. no command needs to be sent")
     }
 }
 
 def setHeatLow(){
-    if (state.fireplaceHeatLow==false){
+    if (! state.heatPowerLow){
         settings.harmonyHub.deviceCommand("low","65347561")
+    }else{
+        logInfo ("$app.label Heat already set to low. no command needs to be sent")
     }
 }
 
 def setHeatHigh(){
-    if (state.fireplaceHeatHigh==false){
+    if (! state.heatPowerHigh){
         settings.harmonyHub.deviceCommand("high","65347561")
+    }else{
+        logInfo ("$app.label Heat already set to high. no command needs to be sent")
     }
 }
-
-def fireplaceColorHandler(evt){
-    changeColor = evt.value
-    logInfo ("fireplace change color $changeColor")
-    state.fireplaceChangeColorOn = (changeColor == "on")
-    changeColor()
-}
-
-def changeColor(){
-        settings.harmonyHub.deviceCommand("clr","65347561")
-}
-
 
 void logInfo(String Msg){
     if (settings?.logEnable !=false){
