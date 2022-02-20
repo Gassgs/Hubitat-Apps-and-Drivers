a27a1a0478a7e270b8f9dc32f7bf578ea38e0004
Gassgs <71162352+Gassgs@users.noreply.github.com>
2022-02-20 11:23:14 -0600

    Create Zigbee Motion Sensor (no temp).groovy

---
+++ Drivers/Zigbee Motion Sensor (no temp).groovy
@@ -0,0 +1,264 @@
+/*
+ *  Zigbee Motion Sensor (no temp)
+ *  for Third Reality and Zemismart/Tuya Motion Sensors
+ *
+ *  Copyright 2018 SmartThings / Modified by Gassgs
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ *  use this file except in compliance with the License. You may obtain a copy
+ *  of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ *
+ *  Change History:
+ *
+ *  V1.0.0  8-10-2021       Added "no temp version" 
+ *  V1.1.0  8-22-2021       Added Battery change date and count
+ *  V1.2.0  11-15-2021      Improved Battery reporting and change date format
+ *  V1.3.0  11-15-2021      Fixed Battery reporting conflict
+ *  V1.4.0  01-21-2022      Driver version for Third Reality and Zemismart/tTya Motion Sensors
+ */
+
+def driverVer() { return "1.4" }
+
+import hubitat.zigbee.clusters.iaszone.ZoneStatus
+
+metadata
+{
+	definition(name: "Zigbee Motion Sensor(no temp)", namespace: "Gassgs", author: "GaryG")
+	{
+		capability "Motion Sensor"
+		capability "Configuration"
+		capability "Battery"
+		capability "Refresh"
+		capability "Sensor"
+        
+        command "batteryChanged"
+        
+        attribute "batteryVoltage","string"
+
+		fingerprint inClusters: "0000,0001,0003,0020,0500,0B05,FC02,FC11", outClusters: "", manufacturer: "Sengled", model: "E1D-G73", deviceJoinName: "Sengled Contact Sensor"
+	}
+
+	preferences{
+		section{
+            input "enableInfo", "bool", title: "Enable info logging?", defaultValue: true, required: false, multiple: false
+			input "enableDebug", "bool", title: "Enable debug logging?", defaultValue: false, required: false, multiple: false
+		}
+	}
+}
+
+
+private List<Map> collectAttributes(Map descMap)
+{
+	List<Map> descMaps = new ArrayList<Map>()
+
+	descMaps.add(descMap)
+
+	if (descMap.additionalAttrs)
+	{
+		descMaps.addAll(descMap.additionalAttrs)
+	}
+
+	return  descMaps
+}
+
+def parse(String description)
+{
+	logDebug "Msg: Description is $description"
+	Map map = zigbee.getEvent(description)
+
+	if (!map)
+	{
+		if (description?.startsWith('zone status'))
+		{
+			map = parseIasMessage(description)
+		}
+		else
+		{
+			Map descMap = zigbee.parseDescriptionAsMap(description)
+
+			if (descMap.cluster == "0001" && descMap.attrId == "0020") {
+				batteryEvent(Integer.parseInt(descMap.value,16))
+			}
+			else if (descMap?.clusterId == "0500" && descMap.attrInt == 0x0002)
+			{
+				def zs = new ZoneStatus(zigbee.convertToInt(descMap.value, 16))
+				map = translateZoneStatus(zs)
+			}
+            
+			else if (descMap?.clusterId == "0001" && descMap.command == "07" && descMap.data[0] == "00")
+			{
+				if (descMap.data[0] == "00")
+				{
+					logDebug "== BATTERY REPORTING CONFIG RESPONSE: success =="
+				}
+				else
+				{
+					logError "!!! BATTERY REPORTING CONFIG FAILED: Error code: ${descMap.data[0]} !!!"
+				}
+			}
+        }
+    }
+	else if (map.name == "batteryVoltage")
+	{
+		map.unit = "V"
+		map.descriptionText = "${device.displayName} battery voltage is ${map.value} volts"
+        logInfo "$device.label battery voltage $map.value"
+        getBatteryResult(map.value)
+	}
+    else if (map.name == "battery") //added for the sengled battery error value with decimal
+	{
+        logDebug "$device.label battery $map.value Logged - not sent to attribute to avoid conflicting reports"
+	}
+    
+    logDebug "Parse returned $map"
+
+	//def result = map ? createEvent(map) : [:]  // disabled sending to attribute for sengled contact sensors
+
+	return result
+}
+
+private Map parseIasMessage(String description) {
+	ZoneStatus zs = zigbee.parseZoneStatus(description)
+
+	translateZoneStatus(zs)
+}
+
+private Map translateZoneStatus(ZoneStatus zs) {
+	return (zs.isAlarm1Set() || zs.isAlarm2Set()) ? getMotionResult('active') : getMotionResult('inactive')
+}
+
+def getBatteryResult(rawValue) {
+	def batteryVolts = $rawValue
+	def minVolts = 1.9
+	def maxVolts = 2.9
+	def pct = (((rawValue - minVolts) / (maxVolts - minVolts)) * 100).toInteger()
+	def batteryValue = Math.min(100, pct)
+    sendEvent("name": "battery", "value": batteryValue, "unit": "%", "displayed": true, isStateChange: true)
+    logInfo "$device.label battery $batteryValue%"
+
+	return
+}
+
+def batteryEvent(rawValue) {
+    log.trace "$device.label $rawValue for battery percent battery event"
+	def batteryVolts = (rawValue / 10).setScale(2, BigDecimal.ROUND_HALF_UP)
+	def minVolts = 19
+	def maxVolts = 29
+	def pct = (((rawValue - minVolts) / (maxVolts - minVolts)) * 100).toInteger()
+	def batteryValue = Math.min(100, pct)
+	if (batteryValue > 0){
+		sendEvent("name": "battery", "value": batteryValue, "unit": "%", "displayed": true, isStateChange: true)
+		sendEvent("name": "batteryVoltage", "value": batteryVolts, "unit": "volts", "displayed": true, isStateChange: true)
+		logInfo "$device.displayName battery changed to $batteryValue%"
+		logInfo "$device.displayName voltage changed to $batteryVolts volts"
+	}
+
+	return
+}
+
+private Map getMotionResult(value) {
+    if (value == "active"){
+        sendEvent(name:"motion",value:"active")
+        logInfo "$device.label Motion Active"
+    }else{
+        sendEvent(name:"motion",value:"inactive")
+        logInfo "$device.label Motion Inactive"
+    }
+}
+
+def refresh() {
+	logInfo "Refreshing Values"
+	def refreshCmds = []
+
+	refreshCmds +=
+		zigbee.readAttribute(zigbee.POWER_CONFIGURATION_CLUSTER, 0x0020) +
+		zigbee.readAttribute(0x0500, 0x0002) +	// IAS Zone Status
+		zigbee.enrollResponse()
+
+	return refreshCmds
+}
+
+def configure() {
+	logDebug "Configuring Reporting..."
+	def configCmds = []
+
+	configCmds +=
+		zigbee.batteryConfig()
+
+	return configCmds
+}
+
+def batteryChanged(){
+    now = new Date()
+    dateFormat = new java.text.SimpleDateFormat("EE MMM d YYYY")
+    timeFormat = new java.text.SimpleDateFormat("h:mm a")
+
+    newDate = dateFormat.format(now)
+    newTime = timeFormat.format(now)
+    
+    timeStamp = newDate + " " + newTime as String
+    
+    state.batteryChanged = "$timeStamp"
+    state.batteryChangedDays = 0
+    initialize()
+}
+
+def addDay(){
+    if (state.batteryChangedDays != null){
+    state.batteryChangedDays = state.batteryChangedDays + 1
+    }
+}
+
+def installed(){
+    batteryChanged()
+    configure()
+}
+
+def updated(){
+    state.DriverVersion = driverVer()
+	initialize()
+    configure()
+}
+
+def initialize(){
+    if (state.batteryChangedDays != null){
+        schedule('0 0 6 * * ?',addDay)
+    }
+	if (enableDebug){
+		logInfo "Verbose logging has been enabled for the next 30 minutes."
+		//runIn(1800, logsOff)
+	}
+}
+
+private logError(msgOut){
+    log.error msgOut
+}
+
+private logWarn(msgOut){
+    log.warn msgOut
+}
+
+private logDebug(msgOut){
+	if (settings.enableDebug){
+		log.debug msgOut
+	}
+}
+
+private logInfo(msgOut){
+    if (settings.enableInfo){
+        log.info msgOut
+    }
+}
+
+def logsOff(){
+    logWarn "debug logging disabled..."
+	device.updateSetting("enableDebug", [value:"false",type:"bool"])
+}
