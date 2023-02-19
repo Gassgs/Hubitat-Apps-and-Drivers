171c63328d44d9ec515af4c292c5b6e38b19bf76
Gassgs <71162352+Gassgs@users.noreply.github.com>
2023-02-19 12:56:31 -0600

    Update Tasmota Child Switch.groovy

--- Drivers/Tasmota Basic Drivers/Tasmota Child Switch.groovy
+++ Drivers/Tasmota Basic Drivers/Tasmota Child Switch.groovy
@@ -18,9 +18,12 @@
  * 
  *  V1.0.0  06-03-2022       first run
  *  V1.1.0  06-22-2022       added toogle command
+ *  V1.2.0  02-10-2023       added flash command
  *  
  * 
  */
+
+def driverVer() { return "1.2" }
 
 metadata {
     definition (name: "Tasmota Child Switch", namespace: "Gassgs", author: "Gary G"){
@@ -29,20 +32,28 @@ metadata {
         capability "Motion Sensor"
         capability "Switch"
         
+        command "flash"
         command "toggle"
     }   
     preferences {
-        input name: "logInfoEnable", type: "bool", title: "Enable text info logging", defaultValue: true
+        input (name: "flashRate", type: "enum", title: "<b>Flash Rate</b>", defaultValue: 750, options: [750:"750ms", 1000:"1s", 2000:"2s", 5000:"5s" ])
+        input name: "logInfoEnable", type: "bool", title: "<b>Enable text info logging</b>", defaultValue: true
     }
 }
 
 def on() {
-     if(logInfoEnable)log.info "$device.label On - Active"
+    if (state.flashing){
+        stopFlash()
+    }
+    if(logInfoEnable)log.info "$device.label On - Active"
     parent.childOn("$device.deviceNetworkId")
 }
 
 def off() {
-     if(logInfoEnable)log.info "$device.label Off - Inactive"
+    if (state.flashing){
+        stopFlash()
+    }
+    if(logInfoEnable)log.info "$device.label Off - Inactive"
     parent.childOff("$device.deviceNetworkId")
 }
 
@@ -53,6 +64,53 @@ def toggle(){
     }else{
         on()
     }
+}
+
+def flash(){
+    if (state.flashing){
+        stopFlash()
+        if (state.restore){
+            on()
+        }
+        else{
+            off()
+        }
+    }
+    else{
+        if (logInfoEnable) log.info "$device.label Flashing Started"
+        state.flashing = true
+        currentStatus = device.currentValue("switch")
+        if (currentStatus == "on"){
+            state.restore = true
+            flashOff()
+        }
+        else{
+            state.restore = false
+            flashOn()
+        }
+    }  
+}
+
+def flashOn(){
+    parent.childOn("$device.deviceNetworkId")
+    runInMillis(flashRate as Integer,flashOff)
+}
+
+def flashOff(){
+    parent.childOff("$device.deviceNetworkId")
+    runInMillis(flashRate as Integer,flashOn)  
+}
+
+def stopFlash(){
+    if (logInfoEnable) log.info "$device.label Flashing Ended"
+    unschedule(flashOn)
+    unschedule(flashOff)
+    state.flashing = false
+}
+
+def updated() {
+    log.info "$device.label updated..."
+    state.DriverVersion=driverVer()
 }
 
 def installed() {
