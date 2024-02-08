b0ee789e52db1146273e3dee4f911ec3b258f99e
Gassgs <71162352+Gassgs@users.noreply.github.com>
2024-02-07 20:16:15 -0600

    Update Tasmota IR Button Controller.groovy

--- Drivers/Tasmota Basic Drivers/Tasmota IR Button Controller.groovy
+++ Drivers/Tasmota Basic Drivers/Tasmota IR Button Controller.groovy
@@ -1,6 +1,6 @@
 /**
  *  Tasmota IR Button Controller
- *  For use with Sofabaton and other IR remotes
+ *  For use with Sofabaton X1 and other Universal IR remotes
  *  
  *  Copyright 2024 Gassgs
  *
@@ -21,9 +21,11 @@
  *
  *  V1.0.0  01-17-2024       first run
  *  V1.1.0  01-27-2024       cleanup added push cmd method
+ *  V1.2.0  02-05-2024       expanded to support up to 30 codes
+ *  V1.3.0  02-07-2024       expanded to support up to 40 codes (needed more)
  */
 
-def driverVer() { return "1.1" }
+def driverVer() { return "1.3" }
 
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
@@ -41,28 +43,52 @@ metadata {
     preferences {
         input name: "deviceIp",type: "string", title: "<b>Tasmota IR Device IP Address</b>", required: true
         input name: "hubIp",type: "string", title: "<b>Hubitat Hub IP Address</b>", required: true
-        input (name: "numberOfButtons", type: "enum", title: "<b>Number Of Buttons</b>", defaultValue: "6", options: ["6", "12", "18"])
+        input (name: "numberOfButtons", type: "enum", title: "<b>Number Of Buttons</b>", defaultValue: "10", options: ["10", "20", "30","40"])
         input name: "button1",type: "string", title: "<b>Button 1 IR Data Value</b>", required: false, defaultValue: "0"
         input name: "button2",type: "string", title: "<b>Button 2 IR Data Value</b>", required: false, defaultValue: "0"
         input name: "button3",type: "string", title: "<b>Button 3 IR Data Value</b>", required: false, defaultValue: "0"
         input name: "button4",type: "string", title: "<b>Button 4 IR Data Value</b>", required: false, defaultValue: "0"
         input name: "button5",type: "string", title: "<b>Button 5 IR Data Value</b>", required: false, defaultValue: "0"
         input name: "button6",type: "string", title: "<b>Button 6 IR Data Value</b>", required: false, defaultValue: "0"
-        if (numberOfButtons == "12" || numberOfButtons == "18"){
-            input name: "button7",type: "string", title: "<b>Button 7 IR Data Value</b>", required: false, defaultValue: "0"
-            input name: "button8",type: "string", title: "<b>Button 8 IR Data Value</b>", required: false, defaultValue: "0"
-            input name: "button9",type: "string", title: "<b>Button 9 IR Data Value</b>", required: false, defaultValue: "0"
-            input name: "button10",type: "string", title: "<b>Button 10 IR Data Value</b>", required: false, defaultValue: "0"
+        input name: "button7",type: "string", title: "<b>Button 7 IR Data Value</b>", required: false, defaultValue: "0"
+        input name: "button8",type: "string", title: "<b>Button 8 IR Data Value</b>", required: false, defaultValue: "0"
+        input name: "button9",type: "string", title: "<b>Button 9 IR Data Value</b>", required: false, defaultValue: "0"
+        input name: "button10",type: "string", title: "<b>Button 10 IR Data Value</b>", required: false, defaultValue: "0"
+        if (numberOfButtons == "20" || numberOfButtons == "30" || numberOfButtons == "40"){
             input name: "button11",type: "string", title: "<b>Button 11 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button12",type: "string", title: "<b>Button 12 IR Data Value</b>", required: false, defaultValue: "0"
-        }
-        if (numberOfButtons == "18"){
             input name: "button13",type: "string", title: "<b>Button 13 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button14",type: "string", title: "<b>Button 14 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button15",type: "string", title: "<b>Button 15 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button16",type: "string", title: "<b>Button 16 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button17",type: "string", title: "<b>Button 17 IR Data Value</b>", required: false, defaultValue: "0"
             input name: "button18",type: "string", title: "<b>Button 18 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button19",type: "string", title: "<b>Button 19 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button20",type: "string", title: "<b>Button 20 IR Data Value</b>", required: false, defaultValue: "0"
+        }
+        if (numberOfButtons == "30" || numberOfButtons == "40"){
+            input name: "button21",type: "string", title: "<b>Button 21 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button22",type: "string", title: "<b>Button 22 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button23",type: "string", title: "<b>Button 23 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button24",type: "string", title: "<b>Button 24 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button25",type: "string", title: "<b>Button 25 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button26",type: "string", title: "<b>Button 26 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button27",type: "string", title: "<b>Button 27 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button28",type: "string", title: "<b>Button 28 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button29",type: "string", title: "<b>Button 29 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button30",type: "string", title: "<b>Button 30 IR Data Value</b>", required: false, defaultValue: "0"
+        }
+        if (numberOfButtons == "40"){
+            input name: "button31",type: "string", title: "<b>Button 31 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button32",type: "string", title: "<b>Button 32 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button33",type: "string", title: "<b>Button 33 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button34",type: "string", title: "<b>Button 34 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button35",type: "string", title: "<b>Button 35 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button36",type: "string", title: "<b>Button 36 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button37",type: "string", title: "<b>Button 37 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button38",type: "string", title: "<b>Button 38 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button39",type: "string", title: "<b>Button 39 IR Data Value</b>", required: false, defaultValue: "0"
+            input name: "button40",type: "string", title: "<b>Button 40 IR Data Value</b>", required: false, defaultValue: "0"
         }
         input name: "refreshEnable",type: "bool", title: "<b>Enable to Refresh every 30mins</b>", defaultValue: true
         input name: "logInfo", type: "bool", title: "<b>Enable info logging</b>", defaultValue: true
@@ -131,7 +157,14 @@ def syncSetup(){
                 "ON IrReceived#Data=" + button3 +" DO webquery http://"+ hubIp + ":39501/ POST 3 ENDON " +
                 "ON IrReceived#Data=" + button4 +" DO webquery http://"+ hubIp + ":39501/ POST 4 ENDON " +
                 "ON IrReceived#Data=" + button5 +" DO webquery http://"+ hubIp + ":39501/ POST 5 ENDON " +
-                "ON IrReceived#Data=" + button6 +" DO webquery http://"+ hubIp + ":39501/ POST 6 ENDON "
+                "ON IrReceived#Data=" + button6 +" DO webquery http://"+ hubIp + ":39501/ POST 6 ENDON " +
+                "ON IrReceived#Data=" + button7 +" DO webquery http://"+ hubIp + ":39501/ POST 7 ENDON " +
+                "ON IrReceived#Data=" + button8 +" DO webquery http://"+ hubIp + ":39501/ POST 8 ENDON " +
+                "ON IrReceived#Data=" + button9 +" DO webquery http://"+ hubIp + ":39501/ POST 9 ENDON " +
+                "ON IrReceived#Data=" + button10 +" DO webquery http://"+ hubIp + ":39501/ POST 10 ENDON " +
+                "ON IrReceived#Data=" + button11 +" DO webquery http://"+ hubIp + ":39501/ POST 11 ENDON " +
+                "ON IrReceived#Data=" + button12 +" DO webquery http://"+ hubIp + ":39501/ POST 12 ENDON " +
+                "ON IrReceived#Data=" + button13 +" DO webquery http://"+ hubIp + ":39501/ POST 13 ENDON " 
             ruleNow = rule.replaceAll("%","%25").replaceAll("#","%23").replaceAll("/","%2F").replaceAll(":","%3A").replaceAll(" ","%20")
             if (logEnable) log.debug "$ruleNow"              
         try {
@@ -147,7 +180,7 @@ def syncSetup(){
             log.warn "Call to on failed: ${e.message}"
         }
     }
-    if (numberOfButtons == "12" || numberOfButtons == "18"){
+    if (numberOfButtons == "20" || numberOfButtons == "30"|| numberOfButtons == "40"){
         runIn(1,syncSetup2)
     }
     else{
@@ -157,12 +190,19 @@ def syncSetup(){
 
 def syncSetup2(){
         if (hubIp){
-            rule = "ON IrReceived#Data=" + button7 +" DO webquery http://"+ hubIp + ":39501/ POST 7 ENDON " +
-                "ON IrReceived#Data=" + button8 +" DO webquery http://"+ hubIp + ":39501/ POST 8 ENDON " +
-                "ON IrReceived#Data=" + button9 +" DO webquery http://"+ hubIp + ":39501/ POST 9 ENDON " +
-                "ON IrReceived#Data=" + button10 +" DO webquery http://"+ hubIp + ":39501/ POST 10 ENDON " +
-                "ON IrReceived#Data=" + button11 +" DO webquery http://"+ hubIp + ":39501/ POST 11 ENDON " +
-                "ON IrReceived#Data=" + button12 +" DO webquery http://"+ hubIp + ":39501/ POST 12 ENDON "
+            rule = "ON IrReceived#Data=" + button14 +" DO webquery http://"+ hubIp + ":39501/ POST 14 ENDON " +
+                "ON IrReceived#Data=" + button15 +" DO webquery http://"+ hubIp + ":39501/ POST 15 ENDON " +
+                "ON IrReceived#Data=" + button16 +" DO webquery http://"+ hubIp + ":39501/ POST 16 ENDON " +
+                "ON IrReceived#Data=" + button17 +" DO webquery http://"+ hubIp + ":39501/ POST 17 ENDON " +
+                "ON IrReceived#Data=" + button18 +" DO webquery http://"+ hubIp + ":39501/ POST 18 ENDON " +
+                "ON IrReceived#Data=" + button19 +" DO webquery http://"+ hubIp + ":39501/ POST 19 ENDON " +
+                "ON IrReceived#Data=" + button20 +" DO webquery http://"+ hubIp + ":39501/ POST 20 ENDON " +
+                "ON IrReceived#Data=" + button21 +" DO webquery http://"+ hubIp + ":39501/ POST 21 ENDON " +
+                "ON IrReceived#Data=" + button22 +" DO webquery http://"+ hubIp + ":39501/ POST 22 ENDON " +
+                "ON IrReceived#Data=" + button23 +" DO webquery http://"+ hubIp + ":39501/ POST 23 ENDON " +
+                "ON IrReceived#Data=" + button24 +" DO webquery http://"+ hubIp + ":39501/ POST 24 ENDON " +
+                "ON IrReceived#Data=" + button25 +" DO webquery http://"+ hubIp + ":39501/ POST 25 ENDON " +
+                "ON IrReceived#Data=" + button26 +" DO webquery http://"+ hubIp + ":39501/ POST 26 ENDON "
             ruleNow = rule.replaceAll("%","%25").replaceAll("#","%23").replaceAll("/","%2F").replaceAll(":","%3A").replaceAll(" ","%20")
             if (logEnable) log.debug "$ruleNow"              
         try {
@@ -178,7 +218,7 @@ def syncSetup2(){
             log.warn "Call to on failed: ${e.message}"
         }
     }
-    if (numberOfButtons == "18"){
+    if (numberOfButtons == "30" || numberOfButtons == "40"){
         runIn(1,syncSetup3)
     }
     else{
@@ -188,12 +228,20 @@ def syncSetup2(){
 
 def syncSetup3(){
         if (hubIp){
-            rule = "ON IrReceived#Data=" + button13 +" DO webquery http://"+ hubIp + ":39501/ POST 13 ENDON " +
-                "ON IrReceived#Data=" + button14 +" DO webquery http://"+ hubIp + ":39501/ POST 14 ENDON " +
-                "ON IrReceived#Data=" + button15 +" DO webquery http://"+ hubIp + ":39501/ POST 15 ENDON " +
-                "ON IrReceived#Data=" + button16 +" DO webquery http://"+ hubIp + ":39501/ POST 16 ENDON " +
-                "ON IrReceived#Data=" + button17 +" DO webquery http://"+ hubIp + ":39501/ POST 17 ENDON " +
-                "ON IrReceived#Data=" + button18 +" DO webquery http://"+ hubIp + ":39501/ POST 18 ENDON "
+            rule = "ON IrReceived#Data=" + button27 +" DO webquery http://"+ hubIp + ":39501/ POST 27 ENDON " +
+                "ON IrReceived#Data=" + button28 +" DO webquery http://"+ hubIp + ":39501/ POST 28 ENDON " +
+                "ON IrReceived#Data=" + button29 +" DO webquery http://"+ hubIp + ":39501/ POST 29 ENDON " +
+                "ON IrReceived#Data=" + button30 +" DO webquery http://"+ hubIp + ":39501/ POST 30 ENDON " +
+                "ON IrReceived#Data=" + button31 +" DO webquery http://"+ hubIp + ":39501/ POST 31 ENDON " +
+                "ON IrReceived#Data=" + button32 +" DO webquery http://"+ hubIp + ":39501/ POST 32 ENDON " +
+                "ON IrReceived#Data=" + button33 +" DO webquery http://"+ hubIp + ":39501/ POST 33 ENDON " +
+                "ON IrReceived#Data=" + button34 +" DO webquery http://"+ hubIp + ":39501/ POST 34 ENDON " +
+                "ON IrReceived#Data=" + button35 +" DO webquery http://"+ hubIp + ":39501/ POST 35 ENDON " +
+                "ON IrReceived#Data=" + button36 +" DO webquery http://"+ hubIp + ":39501/ POST 36 ENDON " +
+                "ON IrReceived#Data=" + button37 +" DO webquery http://"+ hubIp + ":39501/ POST 37 ENDON " +
+                "ON IrReceived#Data=" + button38 +" DO webquery http://"+ hubIp + ":39501/ POST 38 ENDON " +
+                "ON IrReceived#Data=" + button39 +" DO webquery http://"+ hubIp + ":39501/ POST 39 ENDON " +
+                "ON IrReceived#Data=" + button40 +" DO webquery http://"+ hubIp + ":39501/ POST 40 ENDON "
             ruleNow = rule.replaceAll("%","%25").replaceAll("#","%23").replaceAll("/","%2F").replaceAll(":","%3A").replaceAll(" ","%20")
             if (logEnable) log.debug "$ruleNow"              
         try {
@@ -225,7 +273,7 @@ def turnOnRule(){
      } catch (Exception e) {
          log.warn "Call to on failed: ${e.message}"
     }
-    if (numberOfButtons == "12" || numberOfButtons == "18"){
+    if (numberOfButtons == "20" || numberOfButtons == "30" || numberOfButtons == "40"){
         runIn(1,turnOnRule2)
     }
     else{
@@ -246,7 +294,7 @@ def turnOnRule2(){
      } catch (Exception e) {
          log.warn "Call to on failed: ${e.message}"
     }
-    if (numberOfButtons == "18"){
+    if (numberOfButtons == "30" || numberOfButtons == "40"){
         runIn(1,turnOnRule3)
     }
     else{
