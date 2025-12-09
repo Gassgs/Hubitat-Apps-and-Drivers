8e46ce3f64871af8f8ce54a75ffd200d99f98561
Gassgs <71162352+Gassgs@users.noreply.github.com>
2025-12-09 14:41:40 -0600

    Create Garage Door Zwave&Tasmota.groovy

---
+++ Drivers/Garage Door Zwave&Tasmota.groovy
@@ -0,0 +1,498 @@
+/**
+ *  Garage Door -  Z-Wave & Tasmota 
+ *  
+ *  Author: Smartthings
+ *    Mod by Gassgs
+ *
+ *  Ecolink Tilt sensor Gen 5 w/ Tasmota relay to "push" the garage door button
+ *
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Change History:
+ *
+ *  V1.0.0  12-07-2025       Moddified to add motion options
+ */
+
+def driverVer() { return "1.0" }
+
+metadata {
+	definition (
+		name: "Garage Door - Z-Wave & Tasmota", namespace: "Gassgs", author: "SmartThings"
+	) {
+        capability "Actuator"
+        capability "Motion Sensor"
+        capability "Contact Sensor"
+        capability "Signal Strength"
+        capability "Door Control"
+        capability "Garage Door Control"
+		capability "Configuration"
+		capability "Battery"
+		capability "Refresh"
+		capability "Sensor"
+        capability "Tamper Alert"
+        
+        command "batteryChanged"
+        command "ping"
+        
+        attribute "networkStatus","string"
+
+        fingerprint inClusters: "0x86,0x72"
+	    fingerprint mfr:"0109", prod:"2001", model:"0106", deviceJoinName: "Monoprice Door/Window Sensor"
+        fingerprint mfr:"0109", prod:"2022", model:"2201", deviceJoinName: "Monoprice Recessed Door Sensor"
+        fingerprint deviceId: "2201", inClusters:"0x5E,0x86,0x72,0x5A,0x85,0x59,0x73,0x80,0x71,0x84,0x7A,0x98" 
+	}
+	
+	
+	preferences {
+		input "checkinInterval", "number",title: "Minimum Check-in Interval (Hours)",defaultValue: 4,range: "1..167",displayDuringSetup: true, required: false
+		input "reportBatteryEvery", "number", title: "Battery Reporting Interval (Hours)",defaultValue: 6,range: "1..167",displayDuringSetup: true, required: false
+		input "enableExternalSensor", "bool", title: "Enable External Sensor?",defaultValue: false,displayDuringSetup: true, required: false
+        input "timeout", "number", title: "Motion timeout threshold in minutes", defaultValue: 5, required: true, multiple: false
+		input "autoClearTamper", "bool", title: "Automatically Clear Tamper?",defaultValue: false,displayDuringSetup: true, required: false
+        input name: "deviceIp",type: "string", title: "<b>Tasmota Device IP Address</b>", required: true
+        input name: "hubIp",type: "string", title: "<b>Hubitat Device IP Address</b>", required: true
+        input name: "refreshEnable",type: "bool", title: "<b>Enable to ping Tasmota device every 30mins</b>", defaultValue: true
+        input name: "infoEnable", type: "bool", title: "Enable info text logging", defaultValue: true
+		input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+	}
+}
+
+def updated() {	
+	// This method always gets called twice when preferences are saved.
+    if (state.batteryChangedDays != null){
+        schedule('0 0 4 * * ?',addDay)
+    }
+    state.DriverVersion=driverVer()
+    if (refreshEnable){
+        runEvery30Minutes(ping)
+        if (logEnable) log.debug "refresh every 30 minutes scheduled"
+    }else{
+        unschedule(ping)
+        if (logEnable) log.debug "refresh schedule canceled"
+	}
+	if (infoEnable) log.info "updated..."
+    log.warn "debug logging is: ${logEnable == true}"
+    if (logEnable) runIn(1800, logsOff)
+}
+
+def batteryChanged(){
+    now = new Date()
+    dateFormat = new java.text.SimpleDateFormat("EE MMM d YYYY")
+    timeFormat = new java.text.SimpleDateFormat("h:mm a")
+
+    newDate = dateFormat.format(now)
+    newTime = timeFormat.format(now)
+    
+    timeStamp = newDate + " " + newTime as String
+    
+    state.batteryChanged = "$timeStamp"
+    state.batteryChangedDays = 0
+    updated()  
+}
+
+def addDay(){
+    if (state.batteryChangedDays != null){
+    state.batteryChangedDays = state.batteryChangedDays + 1
+    }
+}
+
+def logsOff() {
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable", [value: "false", type: "bool"])
+}
+
+/// Tasmota section/////
+
+def open(){
+    if (state.door == "closed"){
+        openCMD()
+    }else{
+        if (infoEnable) log.info "$device.label Garage Door is already open - not pushing button"
+    }      
+}
+
+def close(){
+    if (state.door == "open"){
+        closeCMD()
+    }else{
+        if (infoEnable) log.info "$device.label Garage Door is already closed - not pushing button"
+    }   
+}
+
+def openCMD() {
+    if (logEnable) log.debug "Sending On Command to [${settings.deviceIp}]"
+    try {
+        httpGet("http://" + deviceIp + "/cm?cmnd=POWER%20On") { resp ->
+            def json = (resp.data)
+            if (logEnable) log.debug "${json}"
+            if (json.POWER == "ON"){
+                sendEvent(name:"door",value:"opening")
+                if (infoEnable) log.info "$device.label Garage Door is opening"
+                if (logEnable) log.debug "Command Success response from Device"
+            }else{
+                if (logEnable) log.debug "Command -ERROR- response from Device- $json"
+            }
+        }
+    } catch (Exception e) {
+        log.warn "Call to on failed: ${e.message}"
+    }
+}
+
+def closeCMD() {
+    if (logEnable) log.debug "Sending On Command to [${settings.deviceIp}]"
+    try {
+        httpGet("http://" + deviceIp + "/cm?cmnd=POWER%20On") { resp ->
+            def json = (resp.data)
+            if (logEnable) log.debug "${json}"
+            if (json.POWER == "ON"){
+                sendEvent(name:"door",value:"closing")
+                if (infoEnable) log.info "$device.label Garage Door is closing"
+                if (logEnable) log.debug "Command Success response from Device"
+            }else{
+                if (logEnable) log.debug "Command -ERROR- response from Device- $json"
+            }
+        }
+    } catch (Exception e) {
+        log.warn "Call to on failed: ${e.message}"
+    }
+}
+
+def ping() {
+    if(settings.deviceIp){
+        if (logEnable) log.debug "Refreshing Device Status - [${settings.deviceIp}]"
+        try {
+           httpGet("http://" + deviceIp + "/cm?cmnd=status%2011") { resp ->
+           def json = (resp.data)
+            if (logEnable) log.debug "${json}"
+               if (json.containsKey("StatusSTS")){
+                   if (logEnable) log.debug "PWR & Wifi status found"
+                   status = json.StatusSTS.POWER as String
+                   signal = json.StatusSTS.Wifi.Signal as String
+                   if (infoEnable) log.info "$device.label Wifi signal strength $signal db"
+                   sendEvent(name:"rssi",value:"${signal}")
+                   sendEvent(name:"networkStatus",value:"online")
+                   if (logEnable) log.debug "$device.label $deviceIp - $status"
+                   }   
+           }
+        }catch (Exception e) {
+            sendEvent(name:"networkStatus",value:"offline")
+            if (infoEnable) log.error "$device.label Unable to connect, device is <b>OFFLINE</b>"
+            log.warn "Call to on failed: ${e.message}"
+        }
+    }
+}
+
+///Z-wave Section///
+
+def configure() {	
+	if (logEnable) log.debug  "configure()"
+	def cmds = []
+	
+	if (!device.currentValue("contact")) {
+		sendEvent(createEventMap("contact", "open", false))
+	}
+	
+	if (!state.isConfigured) {
+        if (logEnable) log.debug  "Waiting 1 second because this is the first time being configured"
+		// Give inclusion time to finish.
+		cmds << "delay 1000"			
+	}
+	
+	initializeCheckin()
+		
+	cmds += delayBetween([
+		wakeUpIntervalSetCmd(checkinIntervalSettingSeconds),
+		externalSensorConfigSetCmd(settings?.enableExternalSensor ?: false),
+		externalSensorConfigGetCmd(),
+		batteryGetCmd()
+	], 100)
+		
+	if (logEnable) log.debug "Sending configuration to device."
+	return cmds
+}
+
+private initializeCheckin() {
+	// Set the Health Check interval so that it can be skipped twice plus 5 minutes.
+	def checkInterval = ((checkinIntervalSettingSeconds * 3) + (5 * 60))
+	
+	sendEvent(name: "checkInterval", value: checkInterval, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+}
+
+
+private getCheckinIntervalSetting() {
+	return (settings?.checkinInterval ?: 6)
+}
+
+private getCheckinIntervalSettingSeconds() {
+	return (checkinIntervalSetting * 60 * 60)
+}
+				
+def parse(String description) {
+	def result = []
+	def cmd = zwave.parse(description, commandClassVersions)
+	if (cmd) {
+		result += zwaveEvent(cmd)
+	}
+	else {
+		if (logEnable) log.debug "Unable to parse description: $description"
+	}
+	
+	return result
+}
+
+def zwaveEvent(hubitat.zwave.commands.securityv1.SecurityMessageEncapsulation cmd) {
+	def result = []
+	def encapCmd = cmd.encapsulatedCommand(commandClassVersions)
+	if (encapCmd) {
+		result += zwaveEvent(encapCmd)
+	}
+	else {
+		if (logEnable) log.warn "Unable to extract encapsulated cmd from $cmd"	
+	}
+	return result
+}
+
+private getCommandClassVersions() {
+	[
+		0x20: 1,  // Basic
+		0x59: 1,  // AssociationGrpInfo
+		0x5A: 1,  // DeviceResetLocally
+		0x5E: 2,  // ZwaveplusInfo
+		0x70: 1,  // Configuration
+		0x71: 3,  // Alarm v1 or Notification v4
+		0x72: 2,  // ManufacturerSpecific*=
+		0x73: 1,  // Powerlevel
+		0x7A: 2,  // FirmwareUpdateMd
+		0x80: 1,  // Battery
+		0x84: 2,  // WakeUp
+		0x85: 2,  // Association
+		0x86: 1,  // Version (2)
+		0x98: 1		// Security
+	]
+}
+
+def zwaveEvent(hubitat.zwave.commands.wakeupv2.WakeUpNotification cmd) {
+	if (logEnable) log.debug "WakeUpNotification: $cmd"
+	def cmds = []
+	
+	if (canSendConfiguration()) {
+		cmds += configure()
+	}
+	else if (canReportBattery()) {
+		cmds << batteryGetCmd()
+	}
+	else {
+		  if (logEnable) log.debug "Skipping battery check because it was already checked within the last $reportEveryHours hours."
+	}
+	
+	if (cmds) {
+		cmds << "delay 5000"
+	}
+	
+	cmds << wakeUpNoMoreInfoCmd()
+	return response(cmds)
+}
+
+private canReportBattery() {
+	def reportEveryHours = settings?.reportBatteryEvery ?: 6
+	def reportEveryMS = (reportEveryHours * 60 * 60 * 1000)
+}
+
+def zwaveEvent(hubitat.zwave.commands.batteryv1.BatteryReport cmd) {
+	def val = (cmd.batteryLevel == 0xFF ? 1 : cmd.batteryLevel)
+	if (val > 100) {
+		val = 100
+	}
+	else if (val < 1) {
+		val = 1
+	}
+    if (infoEnable) log.info "$device.label battery $val %"
+	[
+		createEvent(createEventMap("battery", val, null, "%"))
+	]
+}	
+
+def zwaveEvent(hubitat.zwave.commands.configurationv1.ConfigurationReport cmd) {
+	if (logEnable) log.debug "ConfigurationReport: $cmd"
+	def parameterName
+	switch (cmd.parameterNumber) {
+		case 1:
+			state.enableExternalSensor = (cmd.configurationValue[0] == 0xFF)
+			  if (logEnable) log.debug "External Sensor Enabled: ${state.enableExternalSensor}"
+			break
+		default:	
+			parameterName = "Parameter #${cmd.parameterNumber}"
+	}		
+	if (parameterName) {
+		  if (logEnable) log.debug "${parameterName}: ${cmd.configurationValue}"
+	} 
+	return []
+}
+
+def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
+	  if (logEnable) log.debug "BasicReport: $cmd"	
+	return []
+}
+
+def zwaveEvent(hubitat.zwave.commands.basicv1.BasicSet cmd) {
+	  if (logEnable) log.debug "Basic Set: $cmd"	
+	return []
+}
+
+
+def zwaveEvent(hubitat.zwave.commands.notificationv3.NotificationReport cmd) {
+	def result = []	
+	if (logEnable) log.trace "NotificationReport: $cmd"
+    if (cmd.v1AlarmType == 0x03){
+        result += handleTamperEvent(cmd.event)
+    }
+    else if (cmd.v1AlarmType == 0x02){
+        result += handleContactEvent(cmd.event)
+    }
+	return result
+}
+
+def zwaveEvent(hubitat.zwave.Command cmd) {
+	if (logEnable) log.debug "Unhandled Command: $cmd"
+	return []
+}
+
+private handleContactEvent(event) {
+	if (event == 2) {
+        state.door = "open"
+        sendEvent(name:"contact",value:"open")
+        sendEvent(name:"door",value:"open")
+        sendEvent(name:"motion",value:"active")
+        if (infoEnable) log.info "$device.label contact open"
+        runIn(timeout*60,reset)
+	}
+	else if(event == 0) {
+        unschedule(reset)
+        state.door = "closed"
+        sendEvent(name:"contact",value:"closed")
+        sendEvent(name:"door",value:"closed")
+        sendEvent(name:"motion",value:"inactive")
+        if (infoEnable) log.info "$device.label contact closed"
+	}
+}
+
+def reset(){
+    sendEvent(name:"motion",value:"inactive")
+    if (infoEnable) log.info "$device.label motion inactive after timeout"
+}
+    
+private handleTamperEvent(event) {
+	def result = []
+	def val
+	if (event == 0x03) {
+		val = "detected"
+	}
+	else if (event == 0) {
+		if (settings?.autoClearTamper) {
+			val = "clear"
+		}
+		else {
+			if (logEnable) log.debug "Tamper is Clear"
+		}
+	}
+	if (val) {
+		result << createEvent(createEventMap("tamper", val))
+	}
+	return result
+}
+
+// Resets the tamper attribute to clear and requests the device to be refreshed.
+def refresh() {	
+	if (device.currentValue("tamper") != "clear") {
+		sendEvent(createEventMap("tamper", "clear", false))
+	}
+	else {
+		if (logEnable) log.debug "The configuration and attributes will be refresh the next time the device wakes up.  If you want this to happen immediately, open the back cover of the device, wait until the red light turns solid, and then put the cover back on."
+	}
+}
+
+def createEventMap(eventName, newVal, displayed=null, unit=null) {
+	if (displayed == null) {
+		displayed = (device.currentValue(eventName) != newVal)
+	}
+	if (displayed) {
+		if (infoEnable) log.info "$device.label ${eventName.capitalize()} is ${newVal}"
+	}
+	def eventMap = [
+		name: eventName, 
+		value: newVal, 
+		displayed: displayed,
+		isStateChange: true
+	]
+	if (unit) {
+		eventMap.unit = unit
+	}
+	return eventMap
+}
+
+private wakeUpIntervalSetCmd(val) {
+	if (logEnable) log.debug "wakeUpIntervalSetCmd(${val})"
+	return secureCmd(zwave.wakeUpV2.wakeUpIntervalSet(seconds:val, nodeid:zwaveHubNodeId))
+}
+
+private wakeUpNoMoreInfoCmd() {
+	return secureCmd(zwave.wakeUpV2.wakeUpNoMoreInformation())
+}
+
+private batteryGetCmd() {
+	if (logEnable) log.debug "Requesting battery report"
+	return secureCmd(zwave.batteryV1.batteryGet())
+}
+
+private externalSensorConfigGetCmd() {
+	return configGetCmd(1)
+}
+
+private externalSensorConfigSetCmd(isEnabled) {
+	return configSetCmd(1, 1, (isEnabled ? 0xFF : 0x00))
+}
+
+private configSetCmd(paramNumber, valSize, val) {	
+	if (logEnable) log.debug "Setting configuration param #${paramNumber} to ${val}"
+	return secureCmd(zwave.configurationV1.configurationSet(parameterNumber: paramNumber, size: valSize, configurationValue: [val]))
+}
+
+private configGetCmd(paramNumber) {
+	if (logEnable) log.debug "Requesting configuration report for param #${paramNumber}"
+	return secureCmd(zwave.configurationV1.configurationGet(parameterNumber: paramNumber))
+}
+
+private secureCmd(cmd) {
+	if (zwaveInfo?.zw?.contains("s") || ("0x98" in device.rawDescription?.split(" "))) {
+		return zwave.securityV1.securityMessageEncapsulation().encapsulate(cmd).format()
+	}
+	else {
+		return cmd.format()
+	}	
+}
+
+private canSendConfiguration() {
+	return (!state.isConfigured || state.pendingRefresh != false	|| state.pendingChanges != false)
+}
+
+private convertToLocalTimeString(dt) {
+	def timeZoneId = location?.timeZone?.ID
+	if (timeZoneId) {
+		return dt.format("MM/dd/yyyy hh:mm:ss a", TimeZone.getTimeZone(timeZoneId))
+	}
+	else {
+		return "$dt"
+	}	
+}
+
+private isDuplicateCommand(lastExecuted, allowedMil) {
+	!lastExecuted ? false : (lastExecuted + allowedMil > new Date().time) 
+}
